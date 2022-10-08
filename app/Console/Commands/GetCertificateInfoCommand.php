2726972e8a5a19b881e1e184113ec9be3308b5e3
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-10-08 10:10:58 +0200

    fixes

--- app/Console/Commands/GetCertificateInfoCommand.php
+++ app/Console/Commands/GetCertificateInfoCommand.php
@@ -3,8 +3,12 @@
 namespace App\Console\Commands;
 
 use App\Http\Helpers\CertificateHelper;
+use App\Models\Certificate;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Spatie\SlackAlerts\Facades\SlackAlert;
 
 class GetCertificateInfoCommand extends Command
 {
@@ -19,6 +23,8 @@ class GetCertificateInfoCommand extends Command
 
         //dump($response->body());
 
+        DB::table('certificates')->truncate();
+
         preg_match_all("|([^\n]+.*<l7:User.*)|", $response->body(), $out);
 
         $number = 0;
@@ -26,6 +32,12 @@ class GetCertificateInfoCommand extends Command
         foreach ($out[0] as $line) {
             // prendo quello che c'è dopo id= e poi faccio replace degli ultimi caratteri >' con vuoto
             $userId = str_replace('">', "", substr($line, strpos($line, "id=") + 4, null));
+            if (($userId === "00000000000000000000000000000003")  // admin
+                || ($userId === "da2b9b245c56435a4ae977ac0cc3c47a") // mint_migration
+            )
+            {
+                continue;
+            }
             echo "Getting info for ".$userId."\n";
             $response = Http::withBasicAuth('admin', 'Viola_2013')
                 ->get(
@@ -35,17 +47,41 @@ class GetCertificateInfoCommand extends Command
             $fromCertificateToEnd = substr($response->body(), strpos($response->body(), "<l7:Encoded>") + 12, null);
             $certificate = substr($fromCertificateToEnd, 0, strpos($fromCertificateToEnd, "</l7:Encoded>"));
 
-
+            //dd($certificate);
+            if ($certificate === "") {
+                // utente senza certificato, ignoro
+                continue;
+            }
             $certificateDer = base64_decode($certificate);
 
             $info = openssl_x509_parse(CertificateHelper::der2pem($certificateDer));
 
-            dd($info);
+            try {
+                $cn = $info["subject"]["CN"];
+            }
+            catch (\Exception $e){
+                // alcuni vecchi certificati non hanno il CN
+                $cn = "NOT FOUND - UserID $userId";
+            }
+
+
+            $valid_from = date(DATE_RFC2822, $info['validFrom_time_t']);
+            $valid_to = date(DATE_RFC2822, $info['validTo_time_t']);
+
+            Certificate::create([
+                'common-name' => $cn,
+                'valid_from' => Carbon::make($valid_from),
+                'valid_to' => Carbon::make($valid_to)
+            ]);
+
+
             $number++;
         }
 
-
         echo "Found $number records";
+
+        //SlackAlert::message("Caricati $number certificati da API Gateway su DB locale");
+
     }
 
 
--- app/Http/Helpers/CertificateHelper.php
+++ app/Http/Helpers/CertificateHelper.php
@@ -4,20 +4,18 @@ namespace App\Http\Helpers;
 
 class CertificateHelper
 {
-    public static function der2pem($der_data)
+    public static function der2pem($der_data): string
     {
         $pem = chunk_split(base64_encode($der_data), 64, "\n");
-        $pem = "-----BEGIN CER˙TIFICATE-----\n".$pem."-----END CERTIFICATE-----\n";
-        return $pem;
+        return "-----BEGIN CERTIFICATE-----\n".$pem."-----END CERTIFICATE-----\n";
     }
 
-    public static function pem2der($pem_data)
+    public static function pem2der($pem_data): string
     {
         $begin = "CERTIFICATE-----";
         $end = "-----END";
         $pem_data = substr($pem_data, strpos($pem_data, $begin) + strlen($begin));
         $pem_data = substr($pem_data, 0, strpos($pem_data, $end));
-        $der = base64_decode($pem_data);
-        return $der;
+        return base64_decode($pem_data);
     }
 }
