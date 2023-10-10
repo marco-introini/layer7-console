93dfbcc6612e24c0c7b75320c78e8567c793a953
Marco "Mint" Introini <mint.dev@pm.me>
2023-10-10 16:57:48 +0200

    bugfix

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -6,9 +6,9 @@ cd /var/www/layer7-console
 
 php artisan down
 
+sudo chown -R marco:www-data .
 git pull
 composer install --no-interaction --optimize-autoloader --no-dev
-sudo chown -R www-data:www-data
 npm install
 npm run build
 
@@ -23,6 +23,6 @@ php artisan route:cache
 php artisan view:cache
 php artisan filament:upgrade
 
-php artisan up
+sudo chown -R www-data:www-data .
 
-sudo chown -R www-data:www-data
+php artisan up
