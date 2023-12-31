ec8ccdde2885ad631fa8a4e97997aa11e32f4c9b
Marco "Mint" Introini <mint.dev@pm.me>
2023-12-31 16:32:40 +0100

    Update parameter types in CertificateHelper
    The functions der2pem and pem2der in the CertificateHelper class have been updated to explicitly require string-type parameters. Furthermore, constants in pem2der function have been encapsulated in single quotes for more consistent coding style.

--- app/Http/Helpers/CertificateHelper.php
+++ app/Http/Helpers/CertificateHelper.php
@@ -4,18 +4,20 @@ namespace App\Http\Helpers;
 
 class CertificateHelper
 {
-    public static function der2pem($der_data): string
+    public static function der2pem(string $der_data): string
     {
         $pem = chunk_split(base64_encode($der_data), 64, "\n");
+
         return "-----BEGIN CERTIFICATE-----\n".$pem."-----END CERTIFICATE-----\n";
     }
 
-    public static function pem2der($pem_data): string
+    public static function pem2der(string $pem_data): string
     {
-        $begin = "CERTIFICATE-----";
-        $end = "-----END";
+        $begin = 'CERTIFICATE-----';
+        $end = '-----END';
         $pem_data = substr($pem_data, strpos($pem_data, $begin) + strlen($begin));
         $pem_data = substr($pem_data, 0, strpos($pem_data, $end));
+
         return base64_decode($pem_data);
     }
 }
