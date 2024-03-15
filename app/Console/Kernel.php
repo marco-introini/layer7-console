886d4d61d143a746a4b32ce02969f85508e9d852
Marco "Mint" Introini <mint.dev@pm.me>
2024-03-15 11:18:37 +0100

    Move schedule to console.php as per L11 new defaults

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,9 +10,6 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('gateway:services')->weeklyOn(1,"6:30");
-        $schedule->command('gateway:users')->weeklyOn(1,"6:40");
-        $schedule->command('certificates:check')->weeklyOn(1,"8:00");
     }
 
     protected function commands(): void
--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-
+Schedule::command('gateway:services')
+    ->weeklyOn(1,"6:30");
+Schedule::command('gateway:users')
+    ->weeklyOn(1,"6:40");
+Schedule::command('certificates:check')
+    ->weeklyOn(1,"8:00");
 
