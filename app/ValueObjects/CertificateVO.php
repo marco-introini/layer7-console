c8ca20ae1849db2e665aae440029e5d6b010b36d
Marco "Mint" Introini <mint.dev@pm.me>
2024-03-17 11:19:19 +0100

    Refactor file locations and introduce CertificateVO value object class
    Deleted the CertificateHelper class in the app/Http/Helpers directory and re-introduced it in the App\Helpers directory. Similarly, relocated the XmlHelper class from app/Http/Helpers to App\Helpers directory. Furthermore, a new read-only class, CertificateVO has been created to handle certificate-related operations.

---
+++ app/ValueObjects/CertificateVO.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\ValueObjects;
+
+use App\Helpers\CertificateHelper;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+
+readonly class CertificateVO
+{
+
+    private function __construct(public string $commonName, public Carbon $validFrom, public Carbon $validTo)
+    {
+    }
+
+    public static function fromLayer7EncodedCertificate(string $encodedCert): self
+    {
+        $certificateDer = base64_decode($encodedCert);
+
+        $info = openssl_x509_parse(CertificateHelper::der2pem($certificateDer));
+
+        $cn = $info['subject']['CN'] ?? 'CN NOT FOUND';
+
+        $valid_from = date(DATE_RFC2822, $info['validFrom_time_t']);
+        $valid_to = date(DATE_RFC2822, $info['validTo_time_t']);
+
+        // prima usavo make() -> controllare
+        return new self($cn, Carbon::parse($valid_from), Carbon::parse($valid_to));
+    }
+}
