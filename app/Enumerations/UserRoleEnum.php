51174ea4e79cc5674913bb500efa954931620718
Marco "Mint" Introini <mint.dev@pm.me>
2024-04-11 09:40:05 +0200

    Add companyRoles method to UserRoleEnum
    A new static method, companyRoles, has been added to the UserRoleEnum class. This method returns an array containing the enumeration values for "Company Admin" and "Company User".

--- app/Enumerations/UserRoleEnum.php
+++ app/Enumerations/UserRoleEnum.php
@@ -4,8 +4,16 @@ namespace App\Enumerations;
 
 enum UserRoleEnum: string
 {
-
     case ADMIN = 'Administrator';
     case COMPANY_USER = 'Client Company Employee';
     case COMPANY_ADMIN = 'Client Company Administrator';
+
+    /** return array<string> */
+    public static function companyRoles(): array
+    {
+        return [
+            self::COMPANY_ADMIN->value,
+            self::COMPANY_USER->value,
+        ];
+    }
 }
