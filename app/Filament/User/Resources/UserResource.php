999af1130b6c1790c65248d63edec9e937aece99
Marco "Mint" Introini <mint.dev@pm.me>
2024-04-19 17:24:46 +0200

    Improvements in user section

--- app/Filament/User/Resources/UserResource.php
+++ app/Filament/User/Resources/UserResource.php
@@ -7,11 +7,12 @@ use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
@@ -19,7 +20,31 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $recordTitleAttribute = 'email';
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Basic Information')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->columnSpanFull(),
+                        TextEntry::make('email'),
+                        TextEntry::make('email_verified_at')
+                            ->dateTime('d/m/Y H:i'),
+                    ])->columns(2),
+                Section::make('Access Information')
+                    ->schema([
+                        TextEntry::make('created_at')
+                            ->dateTime('d/m/Y H:i'),
+                        TextEntry::make('updated_at')
+                            ->dateTime('d/m/Y H:i'),
+                    ])->columns(2),
+            ]);
+    }
 
     public static function form(Form $form): Form
     {
@@ -34,12 +59,12 @@ class UserResource extends Resource
                     ->email(),
                 Forms\Components\TextInput::make('password')
                     ->password()
-                    ->required(fn (string $operation): bool => $operation === 'create')
+                    ->required(fn(string $operation): bool => $operation === 'create')
                     ->label('Password')
                     ->confirmed()
                     ->rule(Password::default())
-                    ->dehydrated(fn ($state) => filled($state))
-                    ->dehydrateStateUsing(fn ($state) => Hash::make($state)),
+                    ->dehydrated(fn($state) => filled($state))
+                    ->dehydrateStateUsing(fn($state) => Hash::make($state)),
                 Forms\Components\TextInput::make('password_confirmation')
                     ->password()
                     ->dehydrated(false),
@@ -66,6 +91,7 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -88,6 +114,7 @@ class UserResource extends Resource
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
+            'view' => Pages\ViewUser::route('/{record}'),
         ];
     }
 }
---
+++ app/Filament/User/Resources/UserResource/Pages/ViewUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\User\Resources\UserResource\Pages;
+
+use App\Filament\User\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewUser extends ViewRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+        ];
+    }
+}
