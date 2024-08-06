49e287900b47a444ef3ae0befd6b185114c3415e
Marco "Mint" Introini <mint.dev@pm.me>
2024-08-06 10:43:14 +0200

    Refactor certificate handling in GetTrustedCertsCommand
    Added a new method `fromPemCertificate` in CertificateVO for better modularity. Also corrected and optimized constructor parameter types and formatting for improved readability and consistency.

--- app/ValueObjects/CertificateVO.php
+++ app/ValueObjects/CertificateVO.php
@@ -8,23 +8,24 @@ use Carbon\Carbon;
 readonly class CertificateVO
 {
     public function __construct(public string $commonName,
-        public Carbon|null $validFrom,
-        public Carbon|null $validTo,
-        public string|null $pemCertificate) {}
+        public ?Carbon $validFrom,
+        public ?Carbon $validTo,
+        public ?string $pemCertificate) {}
 
     public static function fromLayer7EncodedCertificate(string $encodedCert): self
     {
-        $certificateDer = base64_decode($encodedCert);
-        $certificatePem = CertificateService::der2pem($certificateDer);
+        return self::fromPemCertificate(CertificateService::der2pem(base64_decode($encodedCert)));
+    }
 
-        $info = openssl_x509_parse(CertificateService::der2pem($certificateDer));
+    public static function fromPemCertificate(string $pemData): self
+    {
+        $info = openssl_x509_parse($pemData);
 
         $cn = $info['subject']['CN'] ?? 'CN NOT FOUND';
 
         $valid_from = date(DATE_RFC2822, $info['validFrom_time_t']);
         $valid_to = date(DATE_RFC2822, $info['validTo_time_t']);
 
-        // prima usavo make() -> controllare
-        return new self($cn, Carbon::parse($valid_from), Carbon::parse($valid_to), $certificatePem);
+        return new self($cn, Carbon::parse($valid_from), Carbon::parse($valid_to), $pemData);
     }
 }
